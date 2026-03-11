41c9915fb1911f50e8bf050c23c66acf6aa9992a
Akos Vaju <vaju.akos@gmail.com>
2026-03-11 20:33:24 +0100

    UI improvements

---
+++ HibaVonal.Client/Helpers/ColorHelper.cs
@@ -0,0 +1,16 @@
+﻿using HibaVonal.Shared.Enum;
+using MudBlazor;
+
+namespace HibaVonal.Client.Helpers
+{
+    public static class ColorHelper
+    {
+        public static Color GetStatusColor(TicketStatus status) => status switch
+        {
+            TicketStatus.New => Color.Info,
+            TicketStatus.InProgress => Color.Warning,
+            TicketStatus.Resolved => Color.Success,
+            _ => Color.Default
+        };
+    }
+}
