777c5768c2427c7b6e02c5a148591232da4350b2
Szonja <Vszonja2003@gmail.com>
2026-03-25 19:58:06 +0100

    Felhasználói lista funkció elkészítve

---
+++ HibaVonal.Shared/UsreDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HibaVonal.Shared.DTO
+{
+   public class UserDTO
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string FullName { get; set; }
+        public string Role { get; set; }
+
+    }
+}
