ba85595ca79071fac8efae1ec22fd626198190ff
Neumann Péter <mail.neumann.peter@gmail.com>
2026-03-25 19:07:35 +0100

    Refactor manager actions into separate controllr and service

---
+++ HibaVonal.Client/Services/ManagerService/IManagerService.cs
@@ -0,0 +1,12 @@
+﻿using HibaVonal.Shared.DTO;
+using HibaVonal.Shared.Enum;
+
+namespace HibaVonal.Client.Services.ManagerService
+{
+    public interface IManagerService
+    {
+        Task<ServiceResponse<List<TicketDTO>>> GetTicketsAsync(bool isCompletedTickets);
+
+        Task<ServiceResponse<bool>> UpdateTicketStatusAsync(int ticketId, TicketStatus status);
+    }
+}
