4f4b396ecf2932d67201b84066c40b7ce1178976
Szonja <Vszonja2003@gmail.com>
2026-04-03 18:07:04 +0200

    Admin funkciók és .gitignore javítása Ákos kérése alapján.

---
+++ HibaVonal.Client/Services/Administrator/AdministratorService .cs
@@ -0,0 +1,33 @@
+﻿
+using HibaVonal.Shared.DTO;
+using System.Net.Http.Json;
+
+namespace HibaVonal.Client.Services.Administrator
+{
+    public class AdministratorService : IAdministratorService
+    {
+        private readonly HttpClient _http;
+
+        public AdministratorService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public async Task<List<EquipmentDTO>> GetEquipmentsAsync()
+        {
+            return await _http.GetFromJsonAsync<List<EquipmentDTO>>("api/admin/equipments");
+        }
+
+        public async Task<bool> AddEquipmentAsync(EquipmentDTO equipment)
+        {
+            var response = await _http.PostAsJsonAsync("api/admin/add", equipment);
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> RemoveEquipmentAsync(int id)
+        {
+            var response = await _http.DeleteAsync($"api/admin/remove/{id}");
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
---
+++ HibaVonal.Client/Services/Administrator/IAdministratorService.cs
@@ -0,0 +1,11 @@
+﻿using HibaVonal.Shared.DTO;
+
+namespace HibaVonal.Client.Services.Administrator
+{
+    public interface IAdministratorService
+    {
+        Task<List<EquipmentDTO>> GetEquipmentsAsync();
+        Task<bool> AddEquipmentAsync(EquipmentDTO equipment);
+        Task<bool> RemoveEquipmentAsync(int id);
+    }
+}
