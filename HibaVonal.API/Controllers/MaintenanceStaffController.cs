b41de934433a4defd991d153ae122dd34c29dfae
totheoryd <totheory.diana@gmail.com>
2026-04-06 19:09:03 +0200

    Add GetAssignedTickets endpoint and service for MaintenanceStaff

---
+++ HibaVonal.API/Controllers/MaintenanceStaffController.cs
@@ -0,0 +1,36 @@
+﻿using HibaVonal.API.Services.MaintenanceStaffService;
+using HibaVonal.API.Services.StudentTicketsService;
+using HibaVonal.Shared.Constants;
+using HibaVonal.Shared.DTO;
+using HibaVonal.Shared.DTO.Ticket;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HibaVonal.API.Controllers
+{
+    [Route("api/maintenanceStaff")]
+    [ApiController]
+    [Authorize(Roles = $"{UserRoles.MaintenanceStaff},{UserRoles.DEV}")]
+
+    public class MaintenanceStaffController : ApiControllerBase
+    {
+        private readonly IMaintenanceStaffService _maintenanceStaffService;
+
+        public MaintenanceStaffController(IMaintenanceStaffService maintenanceStaffService)
+        {
+            _maintenanceStaffService = maintenanceStaffService;
+        }
+
+        [HttpGet("tickets")]
+        public async Task<ServiceResponse<List<TicketDTO>>> GetAssignedTickets([FromQuery] bool isCompleted)
+        {
+            return await _maintenanceStaffService.GetAssignedTickets(CurrentUserId, isCompleted);
+        }
+    }
+
+}
---
+++ HibaVonal.API/Services/MaintenanceStaffService/IMaintenanceStaffService.cs
@@ -0,0 +1,15 @@
+﻿using HibaVonal.Shared.DTO;
+using HibaVonal.Shared.DTO.Ticket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HibaVonal.API.Services.MaintenanceStaffService
+{
+    public interface IMaintenanceStaffService
+    {
+        Task<ServiceResponse<List<TicketDTO>>> GetAssignedTickets(int currentUserId, bool isCompleted);
+    }
+}
