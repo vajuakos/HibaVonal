92257217af728205c4da967e2d1692b79d2f8c00
Akos Vaju <vaju.akos@gmail.com>
2026-04-08 17:54:30 +0200

    Organize usings and authorize attributes

--- HibaVonal.Shared/DTO/Infrastructure/EquipmentRequestDTO.cs
+++ HibaVonal.Shared/DTO/Infrastructure/EquipmentRequestDTO.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HibaVonal.Shared.DTO.Infrastructure
+﻿namespace HibaVonal.Shared.DTO.Infrastructure
 {
     public class EquipmentRequestDTO
     {
